1a4fd62cf86e7be02cab3a30ec0a4c1a7ca645bc
Rodrigo Silva <rdg.hora1@gmail.com>
2023-08-11 11:29:21 -0300

    Botão para iniciar o pomodoro (Working)

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,5 +1,5 @@
 // Importando as dependências necessárias
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useInterval } from '../hooks/use-interval'; // Importando o hook personalizado useInterval
 import { Button } from './button'; // Importando o componente Button
 import { Timer } from './timer'; // Importando o componente Timer
@@ -16,23 +16,45 @@ interface Props {
 export function PomodoroTimer(props: Props): JSX.Element {
   // Definindo o estado do contador principal do Pomodoro usando o valor fornecido nas props
   const [mainTime, setMainTime] = useState(props.pomodoroTime);
+  const [timeCounting, setTimeCounting] = useState(false); // Estado para controlar pausa/play do timer
+  const [working, setWorking] = useState(false); // Estado para controlar fase de trabalho/descanso
+
+  // Efeito para adicionar classe CSS quando a fase de trabalho estiver ativa
+  useEffect(() => {
+    if (working) document.body.classList.add('working'); // Adiciona classe CSS
+    else document.body.classList.remove('working'); // Remove classe CSS
+  }, [working]);
 
   // Utilizando o hook useInterval para atualizar o contador a cada segundo
-  useInterval(() => {
-    setMainTime(mainTime - 1); // Decrementa o tempo a cada chamada do intervalo
-  }, 1000);
+  useInterval(
+    () => {
+      if (timeCounting) setMainTime(mainTime - 1); // Decrementa o tempo apenas se estiver contando
+    },
+    timeCounting ? 1000 : null, // Intervalo de 1 segundo se contando, caso contrário, nulo
+  );
+
+  // Função para configurar a fase de trabalho
+  const configureWorking = () => {
+    setTimeCounting(true); // Inicia a contagem
+    setWorking(true); // Define a fase de trabalho como ativa
+  };
 
   // Renderizando o componente PomodoroTimer
   return (
     <div className="pomodoro">
-      <h2>You are: working</h2>
+      <h2>You are: {working ? 'working' : 'resting'}</h2>
       {/* Renderiza o componente Timer com o tempo atual */}
       <Timer mainTime={mainTime} />
-      {/* Renderiza botões de teste */}
+      {/* Renderiza botões de controle */}
       <div className="controls">
+        <Button text="Work" onClick={() => configureWorking()} />{' '}
+        {/* Botão para iniciar a fase de trabalho */}
         <Button text="teste" onClick={() => console.log(1)} />{' '}
-        <Button text="teste" onClick={() => console.log(1)} />{' '}
-        <Button text="teste" onClick={() => console.log(1)} />
+        {/* Botão de teste */}
+        <Button
+          text={timeCounting ? 'Pause' : 'Play'}
+          onClick={() => setTimeCounting(!timeCounting)} // Alterna entre pausa/play
+        />
       </div>
 
       {/* Exibe detalhes adicionais */}
