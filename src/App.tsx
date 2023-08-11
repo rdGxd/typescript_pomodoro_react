2894cfa5ecd4e02189cb9af11726e44a7b46f7a4
Rodrigo Silva <rdg.hora1@gmail.com>
2023-08-11 11:12:52 -0300

    Configurando CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+// Importando o componente PomodoroTimer do local especificado
 import { PomodoroTimer } from './components/pomodoro-timer';
 
+// Função principal do aplicativo
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <PomodoroTimer defaultPomodoroTime={3600} />
+    <div className="container">
+      {/* Renderiza o componente PomodoroTimer com as propriedades configuradas */}
+      <PomodoroTimer
+        pomodoroTime={1500} // Tempo do Pomodoro em segundos (25 minutos)
+        shortRestTime={300} // Tempo de descanso curto em segundos (5 minutos)
+        longRestTime={900} // Tempo de descanso longo em segundos (15 minutos)
+        cycles={4} // Número de ciclos (Pomodoros) por ciclo completo
+      />
     </div>
   );
 }
 
-export default App;
+export default App; // Exporta a função App como componente principal
--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,32 +1,47 @@
 // Importando as dependências necessárias
 import { useState } from 'react';
-import { useInterval } from '../hooks/use-interval';
-import { Button } from './button';
-import { Timer } from './timer';
+import { useInterval } from '../hooks/use-interval'; // Importando o hook personalizado useInterval
+import { Button } from './button'; // Importando o componente Button
+import { Timer } from './timer'; // Importando o componente Timer
 
 // Definindo a interface para as propriedades do componente
 interface Props {
-  defaultPomodoroTime: number; // Propriedade que define o tempo inicial do contador
+  pomodoroTime: number; // Tempo inicial do contador do Pomodoro
+  shortRestTime: number; // Tempo mínimo de descanso
+  longRestTime: number; // Tempo máximo de descanso
+  cycles: number; // Número de ciclos
 }
 
 // Definindo o componente PomodoroTimer
 export function PomodoroTimer(props: Props): JSX.Element {
-  // Definindo o estado do contador principal do pomodoro usando o valor fornecido nas props
-  const [mainTime, setMainTime] = useState(props.defaultPomodoroTime);
+  // Definindo o estado do contador principal do Pomodoro usando o valor fornecido nas props
+  const [mainTime, setMainTime] = useState(props.pomodoroTime);
 
   // Utilizando o hook useInterval para atualizar o contador a cada segundo
   useInterval(() => {
     setMainTime(mainTime - 1); // Decrementa o tempo a cada chamada do intervalo
   }, 1000);
 
-  // Renderizando o componente com a contagem regressiva formatada em segundosToTime
+  // Renderizando o componente PomodoroTimer
   return (
     <div className="pomodoro">
       <h2>You are: working</h2>
-      <Timer mainTime={mainTime} />
       {/* Renderiza o componente Timer com o tempo atual */}
-      <Button text="teste" onClick={() => console.log(1)} />
-      {/* Renderiza um botão de teste */}
+      <Timer mainTime={mainTime} />
+      {/* Renderiza botões de teste */}
+      <div className="controls">
+        <Button text="teste" onClick={() => console.log(1)} />{' '}
+        <Button text="teste" onClick={() => console.log(1)} />{' '}
+        <Button text="teste" onClick={() => console.log(1)} />
+      </div>
+
+      {/* Exibe detalhes adicionais */}
+      <div className="details">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+      </div>
     </div>
   );
 }
